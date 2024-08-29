d6f75d1e19f67878701a12487967f7d0e2e99f08
Coded-Adil <itsadil2022@gmail.com>
2024-08-30 01:48:43 +0600

    Adding more basic codes about conditions and if else

---
+++ Condationals/if.js
@@ -0,0 +1,40 @@
+/**
+ * Structure
+ * 
+ * if(condition)
+ * {
+ *    action;
+ * }
+ * else
+ * {
+ *    action;
+ * }
+ */
+
+// if(3<10)
+// {
+//     console.log("3 is smaller");
+// }
+
+// var applePrice = 200;
+
+// if (applePrice < 300) {
+//     console.log("Give me 4");
+// }
+// else
+// {
+//     console.log("Is that your final price?");
+// }
+
+var age = 23;
+
+if (age>18) {
+    console.log("You can vote");
+}
+else if (age==18) {
+    console.log("Do you have a voter id card?");
+}
+else
+{
+    console.log("Grow up kid");
+}
---
+++ Condationals/variables.js
@@ -0,0 +1,27 @@
+// Numbers
+var apple = 4;
+// string
+var subject = "CSE";
+// Boolean
+var isCar = true;
+
+/**
+ * NAMING CONVENTION
+ * single word 
+ * no quote
+ * no gap or dash
+ * can not start with a number
+ * can not be a keyword
+ * Prefer Camel case */
+
+var lastExamResult = 89;
+
+/**
+ * OPERATIONS
+ * 
+ * Addition
+ * Subtraction
+ * Mul
+ * mod
+ * div
+ */
