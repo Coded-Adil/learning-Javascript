6bc23383f61754481ac7da861283efc46702e346
Coded-Adil <itsadil2022@gmail.com>
2024-10-05 00:18:39 +0600

    learened more about js

---
+++ API&JS-more/async.js
@@ -0,0 +1,12 @@
+console.log(1);
+console.log(2);
+console.log(3);
+setTimeout(() => {
+    console.log('Murgi');
+}, 4000);
+console.log(5);
+console.log(6);
+
+function doIt(){
+    console.log(4);
+}
